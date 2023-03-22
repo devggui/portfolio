7aadb9f2897497d49a078ad7fb6338cf4425dd21
devggui <devguiet@gmail.com>
2023-03-22 16:35:00 -0300

    feat: skills section

--- src/components/Header/Haeder.tsx
+++ src/components/Header/Haeder.tsx
@@ -2,7 +2,7 @@ import { FaGithub, FaInstagram, FaLinkedin, FaWhatsapp } from 'react-icons/fa';
 
 export function Header() {
   return (
-    <header className="flex flex-col w-full items-center justify-center text-center text-white text-2xl px-20 bg-[var(--dark-blue-header-bg)] bg-[url('../../public/bg-curvy-mobile.svg')] bg-no-repeat bg-bottom">
+    <header className="flex flex-col w-full items-center justify-center text-center text-white text-2xl px-20 bg-[var(--dark-blue-header-bg)] bg-[url('../../public/background/bg-curvy-mobile.svg')] bg-no-repeat bg-bottom">
       <h1 className="w-full py-5 font-semibold">guilhermesilva</h1>
       <div className="bg-gradient-to-t from-[var(--black-op)] to-transparent">
         <nav className="flex flex-row w-full items-center justify-center text-center">
@@ -11,7 +11,7 @@ export function Header() {
           <a href="" className="px-2 animation-delay-1000"><FaWhatsapp /></a>
           <a href="" className="px-2 animation-delay-1000"><FaInstagram /></a>
         </nav>
-        <img src="../../public/profile-photo.png" alt="Foto de perfil" className="w-44" /> 
+        <img src="../../public/photo/profile-photo.png" alt="Foto de perfil" className="w-44" /> 
       </div>
     </header>
   )
--- src/components/Main/Sections/Skills.tsx
+++ src/components/Main/Sections/Skills.tsx
@@ -1,9 +1,36 @@
 export function Skills() {
   return (
     <section className="flex flex-col items-center justify-center text-center">
-      <img src="" alt="" />
-      <h1 className="text-white text-3xl font-bold">HTML</h1>
-      <p className="text-white text-base font-light antialiased">4 Years Experience</p>
+      <div className="flex flex-col items-center justify-center text-center mb-8">
+        <img src="../../../public/icons/html.svg" alt="" />
+        <h1 className="text-white text-3xl font-bold mt-1">HTML</h1>
+        <p className="text-white text-base font-light antialiased">0-1 Year Experience</p>
+      </div>
+      <div className="flex flex-col items-center justify-center text-center mb-8">
+        <img src="../../../public/icons/css.svg" alt="" />
+        <h1 className="text-white text-3xl font-bold mt-1">CSS</h1>
+        <p className="text-white text-base font-light antialiased">0-1 Year Experience</p>
+      </div>      
+      <div className="flex flex-col items-center justify-center text-center mb-8">
+        <img src="../../../public/icons/javascript.svg" alt="" />
+        <h1 className="text-white text-3xl font-bold mt-1">Javascript</h1>
+        <p className="text-white text-base font-light antialiased">0-1 Year Experience</p>
+      </div>      
+      <div className="flex flex-col items-center justify-center text-center mb-8">
+        <img src="../../../public/icons/reactjs.svg" alt="" />
+        <h1 className="text-white text-3xl font-bold mt-1">React.js</h1>
+        <p className="text-white text-base font-light antialiased">0-1 Year Experience</p>
+      </div>
+      <div className="flex flex-col items-center justify-center text-center mb-8">
+        <img src="../../../public/icons/typescript.svg" alt="" />
+        <h1 className="text-white text-3xl font-bold mt-1">Typescript</h1>
+        <p className="text-white text-base font-light antialiased">0-1 Year Experience</p>
+      </div>      
+      <div className="flex flex-col items-center justify-center text-center">
+        <img src="../../../public/icons/tailwind.svg" alt="" />
+        <h1 className="text-white text-3xl font-bold mt-1">Tailwind CSS</h1>
+        <p className="text-white text-base font-light antialiased">0-1 Year Experience</p>
+      </div>
     </section>
   )
 }
