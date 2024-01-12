71410910f9eafaa37a1347652080f98ab38800ab
devggui <devguiet@gmail.com>
2024-01-12 15:09:08 -0300

    chore: updated curriculum

--- src/components/Buttons/DowloadCV.tsx
+++ src/components/Buttons/DowloadCV.tsx
@@ -1,3 +1,3 @@
 export function DownloadCV() {
-  return <a href="/pdf/CV-Guilherme-Silva.pdf" target="_blank" className="hover-fill text-white text-2xl font-semibold pb-1 border-b-2 border-[var(--cyan-gradient-from)] antialiased" data-txthover="DOWNLOAD CV">DOWNLOAD CV</a>
+  return <a href="/pdf/Curriculo-Guilherme-Silva.pdf" target="_blank" className="hover-fill text-white text-2xl font-semibold pb-1 border-b-2 border-[var(--cyan-gradient-from)] antialiased" data-txthover="DOWNLOAD CV">DOWNLOAD CV</a>
 }
